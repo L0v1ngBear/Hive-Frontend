bfc8c8cf8668bf9a53ece7eb6129c36b097a41b6
曹立峰 <caolf56397@hundsun.com>
2026-04-01 19:16:09 +0800

    配置文件修改

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // app.js
 App({
+  onLaunch() {
+    // 监听全局报错
+    wx.onError((err) => {
+      console.log('【全局捕获的报错】:', err);
+    });
+
+    // 监听 Promise 中未被 catch 的 rejection (这个最容易引发 [object Object])
+    wx.onUnhandledRejection((res) => {
+      console.log('【未处理的 Promise 异常】:', res.reason);
+    });
+  },
   globalData: {
     baseUrl: 'https://www.dtvl.top' // 你的后端服务器地址
   }
